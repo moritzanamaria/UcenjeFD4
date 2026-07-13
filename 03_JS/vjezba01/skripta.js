7838c6ba1882c8b690880547e86236856a5846b9
moritzanamaria <moritzanamaria@gmail.com>
2026-07-13 20:58:58 +0200

    03_JS Uvod u programiranje

---
+++ 03_JS/vjezba01/skripta.js
@@ -0,0 +1,16 @@
+// alert('Hello world 1')
+
+// confirm('Hello world 2, pitanje')
+
+document.write('Hello world 3')
+
+document.getElementById('ispis').innerText = 'Hello world 7'
+document.getElementById('ispis').style.color = 'red'
+
+document.getElementById('ge').innerHTML = `
+
+   <a href="#">
+   Ovo je generirana poveznica
+   </a>
+
+`
